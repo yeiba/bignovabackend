72b3ae973d952e0d644856b6139b6b8b67ccb1d8
Jacob_Belhardi <yaakoubbelhardi@gmail.com>
2025-05-03 22:30:53 +0100

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-import startServer from "./server/server";
+import dotenv from "dotenv";
+import expressApp from "./server/expressApp";
+import config from "./config";
+import { console } from "inspector";
+dotenv.config(); // Load environment variables
 
-startServer(); // Start the application
+// Constants
+const PORT = config.PORT;
+
+// Start the Express server
+
+expressApp.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
---
+++ src/server/index.ts
@@ -0,0 +1,4 @@
+// src/index.ts
+import expressApp from "./expressApp";
+
+export default expressApp;
