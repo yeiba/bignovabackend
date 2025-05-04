005ded834e487890ea719a806afac2cff9655eac
Jacob_Belhardi <yaakoubbelhardi@gmail.com>
2025-05-04 06:18:00 +0100

    add render domain for front end to backend cors origin

--- src/server/expressApp.ts
+++ src/server/expressApp.ts
@@ -19,7 +19,7 @@ import { ErrorControllers } from "../controllers/error.controllers";
 // CORS Configuration
 const corsOptions = {
   credentials: true, // Allow credentials such as cookies or authorization headers
-  origin: "http://localhost:3000", // Allow all origins (can be restricted for production)
+  origin: ["http://localhost:3000", "https://bignovabackend.onrender.com"], // Allow all origins (can be restricted for production)
 };
 
 const app = express(); // Create Express application
